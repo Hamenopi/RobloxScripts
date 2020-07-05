15851930b1b4aa19785e0a7a6190236540ff0b83
Hamenopi <justin.p.hamilton@gmail.com>
2020-07-05 08:05:37 -0500

    Add Comments

--- SimpleCheckpointScript.lua
+++ SimpleCheckpointScript.lua
@@ -1,12 +1,12 @@
 --Add leaderstats/Stage to Player upon connect
 game.Players.PlayerAdded:Connect(function(Player)
   
-  -- leaderstats Folder
+  -- leaderstats Folder (under Player)
   local leaderstats = Instance.new("Folder")
   leaderstats.Name = "leaderstats"
   leaderstats.Parent = Player
   
-  -- stage intVal
+  -- stage intVal (under leaderstat folder)
   local stage = Instance.new("IntValue")
   stage.Name = "Stage"
   stage.Value = 1
@@ -14,13 +14,13 @@ game.Players.PlayerAdded:Connect(function(Player)
   -- Teleport to Stage's spawn point
   Player.CharacterAdded:Connect(function(Character
 
-    -- wait for Char to load
+    -- wait for characther model to load
     repeat wait() until Player.Character ~= nil
     
-    -- get Stage(stage.Value)
+    -- get matching stage object from (stage.Value)
     local thisStage = workspace.Stage:FindFirstChild(stage.Value)
     
-    -- move to Stage(stage.Value)
+    -- move to character to above stage object(stage.Value)
     Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(thisStage.Position _ Vector3.new(0,2,0))
   end)
 end)
