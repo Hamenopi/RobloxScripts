93626ff3b303fa852e6d967d9b752c2de671364c
Hamenopi <justin.p.hamilton@gmail.com>
2020-07-05 14:12:15 -0500

    Update SaveDataHandler.lua

--- SaveDataHandler.lua
+++ SaveDataHandler.lua
@@ -2,18 +2,14 @@ local Players = game:GetService("Players")
 local DataStoreService = game:GetService("DataStoreService")
 local SaveDataStore = DataStoreService:GetDataStore("SaveData")
  
- -- Sets SaveData
+ -- Saves Stage data to DataStore
 local function SavePlayerData(player)
    
-    local success,errormsg = pcall(function()
-   
-        local SaveData = {}
-       
-        for i,stats in pairs(player.leaderstats:GetChildren()) do
-           
+    local success,errormsg = pcall(function()   
+        local SaveData = {}       
+        for i,stats in pairs(player.leaderstats:GetChildren()) do           
             SaveData[stats.Name] = stats.Value
-        end
-        
+        end        
         SaveDataStore:SetAsync(player.UserId,SaveData)
     end)
    
@@ -21,52 +17,49 @@ local function SavePlayerData(player)
         return errormsg
     end        
 end
- 
- -- creates \leaderstats\Stage
+  
 Players.PlayerAdded:Connect(function(player)
-   
-    local Stats = Instance.new("Folder")
-    Stats.Name = "leaderstats"
-    Stats.Parent = player
-   
-    local Stage = Instance.new("IntValue")
-    Stage.Name = "Stage"
-    Stage.Parent = Stats
-    Stage.Value = 1
-   
-   -- Get's table from SavaData
-    local Data = SaveDataStore:GetAsync(player.UserId)
-   
-   -- Load player data
-    if Data then
-           
-        --print(Data.Stage)
        
-        for i,stats in pairs(Stats:GetChildren()) do
-           
+    -- Gets SavaData from DataStore
+    local data = SaveDataStore:GetAsync(player.UserId)
+
+    -- creates \leaderstats\stage
+    local leaderstats = Instance.new("Folder")
+    leaderstats.Name = "leaderstats"
+    leaderstats.Parent = player
+
+    local stage = Instance.new("IntValue")
+    stage.Name = "Stage"
+    stage.Parent = stats
+    stage.Value = 1
+  
+    -- Load SaveData
+    if data then           
+      print(player.Name .. " is on stage " .. data.Stage)       
+      -- future proofing
+      for i, stats in pairs(Stats:GetChildren()) do           
             stats.Value = Data[stats.Name]     
-        end        
+      end        
         
-    else       
-        
-        -- Ne wplayer
-        print(player.Name .. " has no data.")          
+    else        
+      -- Newplayer
+      print(player.Name .. " is on stage 1")          
         
     end
    
    -- on character model load
     player.CharacterAdded:Connect(function(character)
        
-        local Humanoid = character:WaitForChild("Humanoid")
-        local Torso = character:WaitForChild("HumanoidRootPart")
+        local humanoid = character:WaitForChild("Humanoid")
+        local torso = character:WaitForChild("HumanoidRootPart")
        
         wait()
        
-        if Torso and Humanoid then
-            if Stage.Value ~= 0 then
+        if torso and humanoid then
+            if stage.Value ~= 0 then
                
-                local StagePart = workspace.Stages:FindFirstChild(Stage.Value)
-								Torso.CFrame = StagePart.CFrame + Vector3.new(0,3,0)                   
+                local stagePart = workspace.Stages:FindFirstChild(stage.Value)
+								torso.CFrame = stagePart.CFrame + Vector3.new(0,5,0)                   
             end
         end
     end)       
@@ -84,12 +77,12 @@ end)
 -- save data on game close
 game:BindToClose(function()
     -- for any players left in game
-    for i,player in pairs(Players:GetPlayers()) do       
+    for i, player in pairs(Players:GetPlayers()) do       
       -- save player's stage
         local errormsg = SavePlayerData(player)
         if errormsg then
             warn(errormsg)
         end        
     end
-    wait(0.1)
+    wait(1)
 end)
