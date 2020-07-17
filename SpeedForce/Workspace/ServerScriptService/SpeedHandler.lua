52875de19f718ae739796353bc4740afef5f0cec
Hamenopi <justin.p.hamilton@gmail.com>
2020-07-17 11:38:50 -0500

    Create SpeedHandler.lua

---
+++ SpeedForce/Workspace/ServerScriptService/SpeedHandler.lua
@@ -0,0 +1,28 @@
+--[[
+Handles when SetSpeed in invoked
+]]
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Constants = ReplicatedStorage.Constants
+local Common = ReplicatedStorage.Common
+local Players = game:GetService("Players")
+
+Players.PlayerAdded:Connect(function(player)
+
+	 player.CharacterAdded:Connect(function(character)
+
+		local humanoid = character:WaitForChild("Humanoid")
+
+		while true do
+
+			wait(Constants.Duration)
+
+			if Common.IsMoving(humanoid) then
+
+				local newSpeed = Common.SetSpeed(player.hiddenstats.Speed.Value + 1)
+
+				player.hiddenstats.Speed.Value = newSpeed
+
+			end -- if
+		end -- while
+	end) -- player.CharacterAdded
+end) -- Players.PlayerAdded
